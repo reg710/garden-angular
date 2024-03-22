0692f5f2ed7a5ed778c1f88289b045a2c86f3ec9
reg710 <reg710@gmail.com>
2024-03-22 16:24:57 -0400

    Generate environments and include test message for seeing which configuration is loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LocationSearchComponent } from './views/location-search/location-search.component';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,9 @@ import { LocationSearchComponent } from './views/location-search/location-search
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor() {
+    console.log(environment.message);
+  }
+
   title = 'garden-angular';
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:5200',
+    message: 'you are in dev'
+};
