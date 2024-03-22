c9f0d86a3186b8daff01da4d808ce92c04ddb7f6
reg710 <reg710@gmail.com>
2024-03-22 19:54:21 -0400

    Update layout plan and center text and form fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LocationSearchComponent } from './views/location-search/location-search.component';
 import { environment } from '../environments/environment';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
+    RouterOutlet,
     LocationSearchComponent,
+    MatCardModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
