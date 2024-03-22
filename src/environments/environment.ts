fc21a2a38e814a4f0978141076b01b7e4934ca09
reg710 <reg710@gmail.com>
2024-03-22 16:36:52 -0400

    Fix url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://garden-backend-qer0.onrender.com/',
+    apiUrl: 'https://garden-backend-qer0.onrender.com',
     message: 'look here is prod'
 };
