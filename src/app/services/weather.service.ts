417986cd98da5dc0aa544803cac6b196c1b43bc4
reg710 <reg710@gmail.com>
2024-03-22 16:30:14 -0400

    Use environments apiUrl in weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WEATHER } from './temp-weather'
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  // TODO look up how to move this to environments
-  // Or environments equivalent in this newer version of Angular
-  private url = 'http://localhost:5200';
+  private url = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) { }
 
