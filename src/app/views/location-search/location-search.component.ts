b4c238d26520d03ba324b4f68da9a51dd9af1f53
reg710 <reg710@gmail.com>
2024-03-22 19:20:06 -0400

    WIP Create background view and initial form view

--- src/app/views/location-search/location-search.component.ts
+++ src/app/views/location-search/location-search.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { WeatherService } from '../../services/weather.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-location-search',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './location-search.component.html',
   styleUrl: './location-search.component.scss'
 })
 export class LocationSearchComponent {
 
-  constructor(private weatherService: WeatherService) {}
+  // TODO add regex validator to limit to 5 digit pattern
+  zipFormControl = new FormControl('', [Validators.required])
+
+  constructor(private weatherService: WeatherService) { }
 
   public fetchWeather() {
-    this.weatherService.getWeather()
-      .subscribe((response) => {
-        console.log(response);
-      })
+    // this.weatherService.getWeather()
+    //   .subscribe((response) => {
+    //     console.log(response);
+    //   })
   }
 }
