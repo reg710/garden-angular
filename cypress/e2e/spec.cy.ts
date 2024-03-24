e27135fdfd1d91eed051ed65a9780bdec9bf9780
reg710 <reg710@gmail.com>
2024-03-24 12:29:26 -0400

    Add initial tests and data selectors. Intercept test failing on timeout.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,53 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+// TODO move to fixtures and fix import error
+const weatherResponse = 
+  {
+    "locationName": "15201, USA",
+    "data": [
+        {
+            "datetimeStr": "2024-03-20T00:00:00-04:00",
+            "precipitation": 1
+        },
+        {
+            "datetimeStr": "2024-03-21T00:00:00-04:00",
+            "precipitation": 0.2
+        },
+        {
+            "datetimeStr": "2024-03-22T00:00:00-04:00",
+            "precipitation": 0.09
+        }
+    ]
+}
+
+describe('Garden App', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  });
+
+  it('has page title', () => {
+    cy.get('[data-cy="page-title"]').should('exist')
+  });
+
+  it('has zip input field', () => {
+    cy.get('[data-cy="zip-input"]').type('15201')
+  });
+
+  describe.skip('Weather API', () => {
+    const zip = '15201';
+    // TODO add environment url
+    beforeEach(() => {
+      cy.intercept(
+        "GET",
+        'http://localhost:5200/weather/' + zip,
+        (req) => {
+          req.reply({body: weatherResponse, statusCode: 200})
+        }
+      ).as("WeatherLookupRequest");
+      cy.get('[data-cy="zip-input"]').type(zip);
+      cy.get('[data-cy="zip-input"]').click();
+      cy.wait("@WeatherLookupRequest");
+    });
+    it('shows decision', () => {
+      cy.get(('[data-cy="decision"]')).should('exist')
+    })
+  });
 })
