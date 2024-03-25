f0918922f9fbcd65a06f847292bf843956bb1098
reg710 <reg710@gmail.com>
2024-03-24 21:28:39 -0400

    Update decision style to include long answer toggle, currently have temp long answer text

--- src/app/views/decision/decision.component.ts
+++ src/app/views/decision/decision.component.ts
@@ -1,22 +1,28 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { IWeatherData, WeatherReport } from '../../models/weather.model';
+import { WEATHER } from '../../models/temp-weather';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-decision',
   standalone: true,
   imports: [
     MatCardModule,
+    MatSlideToggleModule,
+    FormsModule,
   ],
   templateUrl: './decision.component.html',
   styleUrl: './decision.component.scss'
 })
 export class DecisionComponent implements OnChanges {
 
-  @Input() weatherReport?: WeatherReport;
+  @Input() weatherReport?: WeatherReport = WEATHER;
 
-  showDecision: boolean = false;
-  water?: boolean;
+  showLongAnswer: boolean = false;
+  shortAnswer: string = '';
+  longAnswer: string = '';
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['weatherReport'] && changes['weatherReport'].currentValue && this.weatherReport) {
@@ -28,13 +34,33 @@ export class DecisionComponent implements OnChanges {
     return data.reduce((total, day) => day.precipitation + total, 0);
   }
 
+  sayYes() {
+    this.shortAnswer = "Yes!";
+    this.longAnswer = "Long way of saying yes";
+  }
+
+  sayMaybe() {
+    this.shortAnswer = "Maybe...";
+    this.longAnswer = "Long way of saying maybe";
+  }
+
+  sayNo() {
+    this.shortAnswer = "No.";
+    this.longAnswer = "Long way of saying no";
+  }
+
   makeWateringDecision(report: WeatherReport) {
+    this.shortAnswer = '';
+    this.longAnswer = '';
     let totalRainfall = this.sumRainfall(report.data);
     console.log(totalRainfall);
-    if (totalRainfall > 2) {
-      this.water = false;
+
+    if (totalRainfall >= 1.5) {
+      this.sayNo();
+    } else if (totalRainfall < 1.5 && totalRainfall >= 1) {
+      this.sayMaybe();
+    } else {
+      this.sayYes();
     }
-    this.water = true;
-    this.showDecision = true;
   }
 }
