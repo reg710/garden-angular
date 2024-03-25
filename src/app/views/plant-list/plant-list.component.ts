2221983fd50336fef6fe3f1b7eea8073070a62af
reg710 <reg710@gmail.com>
2024-03-25 18:17:12 -0400

    Add material table with temp data

--- src/app/views/plant-list/plant-list.component.ts
+++ src/app/views/plant-list/plant-list.component.ts
@@ -1,19 +1,58 @@
 import { Component } from '@angular/core';
 import { PlantService } from '../../services/plant.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { IPlant } from '../../models/plant.model';
 
+
+const TEMP_PLANTS: IPlant[] = [
+  {
+    name: 'Beans',
+    wateringNeeds: 'high'
+  },
+  {
+    name: 'Asparagus',
+    wateringNeeds: 'low'
+  }
+]
 @Component({
   selector: 'app-plant-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatCheckboxModule],
   templateUrl: './plant-list.component.html',
   styleUrl: './plant-list.component.scss'
 })
 export class PlantListComponent {
 
-  constructor(private plantService: PlantService) {}
+  displayedColumns: string[] = ['select', 'name', 'wateringNeeds'];
+  dataSource = new MatTableDataSource<IPlant>(TEMP_PLANTS);
+  selection = new SelectionModel<IPlant>(true, []);
+
+  constructor(private plantService: PlantService) { }
+
+  // Checking the number selected against total number of rows in data source
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  // Either selects all or unchecks all
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  // TODO track selected ids
+
 
   fetchAllPlants() {
-    this.plantService.getAllPlants() 
+    this.plantService.getAllPlants()
       .subscribe((response) => {
         console.log(response);
       })
