b1f8d34daa56a3482fffd8c31318132cfb425206
reg710 <reg710@gmail.com>
2024-03-24 22:11:12 -0400

    Display daily amounts of precipitation in long answer

--- src/app/views/decision/decision.component.ts
+++ src/app/views/decision/decision.component.ts
@@ -4,11 +4,13 @@ import { IWeatherData, WeatherReport } from '../../models/weather.model';
 import { WEATHER } from '../../models/temp-weather';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-decision',
   standalone: true,
   imports: [
+    CommonModule,
     MatCardModule,
     MatSlideToggleModule,
     FormsModule,
@@ -23,6 +25,7 @@ export class DecisionComponent implements OnChanges {
   showLongAnswer: boolean = false;
   shortAnswer: string = '';
   longAnswer: string = '';
+  totalRainfall: number = 0;
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['weatherReport'] && changes['weatherReport'].currentValue && this.weatherReport) {
@@ -36,28 +39,28 @@ export class DecisionComponent implements OnChanges {
 
   sayYes() {
     this.shortAnswer = "Yes!";
-    this.longAnswer = "Long way of saying yes";
+    this.longAnswer = "Over the last week there's been less than an inch of rainfall so those plants are probably thirsty";
   }
 
   sayMaybe() {
     this.shortAnswer = "Maybe...";
-    this.longAnswer = "Long way of saying maybe";
+    this.longAnswer = "Between one to two inches is the recommended amount of water in a week. Although it's above that one inche threshold, it may be worth checking the soil just to be sure. Don't want to lose those beans.";
   }
 
   sayNo() {
     this.shortAnswer = "No.";
-    this.longAnswer = "Long way of saying no";
+    this.longAnswer = "Between one to two inches is the recommended amount of water in a week. You don't want to fall into the trap of over watering.";
   }
 
   makeWateringDecision(report: WeatherReport) {
     this.shortAnswer = '';
     this.longAnswer = '';
-    let totalRainfall = this.sumRainfall(report.data);
-    console.log(totalRainfall);
+    this.totalRainfall = this.sumRainfall(report.data);
+    console.log(this.totalRainfall);
 
-    if (totalRainfall >= 1.5) {
+    if (this.totalRainfall >= 1.5) {
       this.sayNo();
-    } else if (totalRainfall < 1.5 && totalRainfall >= 1) {
+    } else if (this.totalRainfall < 1.5 && this.totalRainfall >= 1) {
       this.sayMaybe();
     } else {
       this.sayYes();
